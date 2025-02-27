cc9b2714dcb7fab416a218d4500ecb1b66ed110e
Jaapodaca <jaapodaca15@gmail.com>
2025-02-27 14:23:53 -0700

    code 5

--- Notes.py
+++ Notes.py
@@ -15,11 +15,14 @@ df = pd.read_csv("./big-mac-full-index.csv")
 #     if row['dollar_price'] < 1:
 #         print(row['dollar_price'])
 
-smol_df = df.query("index < 10")
+# smol_df = df.query("index < 10")
 
-def get_new_country_name(row):
-    new_country_name = f'{row['name']} ({row['iso_a3"]})']})'
-    return new_country_name
+# def get_new_country_name(row):
+#     new_country_name = f'{row['name']} ({row['iso_a3"]})']})'
+#     return new_country_name
 
-smol_df['name'] = smol_df.apply(get_new_country_name, axis=1)
-print(smol_df)
+# smol_df['name'] = smol_df.apply(get_new_country_name, axis=1)
+# print(smol_df)
+
+file_name = "./big-mac-full-index.csv"
+df = pd.read_csv(file_name)
