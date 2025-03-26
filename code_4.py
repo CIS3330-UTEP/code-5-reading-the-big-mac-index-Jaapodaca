d30065cfe7072b473e6b40b6a791d69e8ed827dd
Jaapodaca <jaapodaca15@gmail.com>
2025-03-26 14:33:34 -0600

    I need to resubmit

--- code_4.py
+++ code_4.py
@@ -1,14 +1,14 @@
-import csv
 import pandas as pd
-big_mac_file = './big-mac-full-index.csv'
-df = pd.read_csv(big_mac_file)
 
-def get_big_mac_price_by_year(year, country_code):
-    data = df[(df['date'].str.contains(str(year))) & (df['iso_a3'].str.lower() == country_code.lower())]
+filename = "big-mac-full-index.csv"
+df = pd.read_csv(filename)
+
+def get_big_mac_price_by_year(year, country):
+    data = df[(df['date'].str.contains(str(year))) & (df['iso_a3'].str.lower() == country.lower())]
     return round(data['dollar_price'].mean(), 2)
 
-def get_big_mac_price_by_country(country_code):
-    data = df[df['iso_a3'].str.lower() == country_code.lower()]
+def get_big_mac_price_by_country(country):
+    data = df[df['iso_a3'].str.lower() == country.lower()]
     return round(data['dollar_price'].mean(), 2)
 
 def get_the_cheapest_big_mac_price_by_year(year):
@@ -22,10 +22,7 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     return f"{expensive['name']}({expensive['iso_a3']}): ${round(expensive['dollar_price'], 2)}"
 
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
-
-print(get_big_mac_price_by_year(2010, 'usa'))
-print(get_big_mac_price_by_country('jpn'))
-print(get_the_cheapest_big_mac_price_by_year(2015))
-print(get_the_most_expensive_big_mac_price_by_year(2015)) 
-
+    print(get_big_mac_price_by_year(2010, 'usa'))
+    print(get_big_mac_price_by_country('jpn'))
+    print(get_the_cheapest_big_mac_price_by_year(2015))
+    print(get_the_most_expensive_big_mac_price_by_year(2015))
